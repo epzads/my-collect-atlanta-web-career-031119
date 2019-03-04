d1383f3bf5f8e15204313658a1f578bc826da57f
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 20:24:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
   i = 0
+  new_array = []
   while i < collection.length
-    yield(collection[i])
+    newarray << yield(collection[i])
     i = i + 1
   end
-  collection
+  return newarray
 end    
 
